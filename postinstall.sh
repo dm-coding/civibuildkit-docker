57eb93a0866b82e595ca0f34237fc24486c02b28
Daniel James <daniel.james@mailbox.org>
2016-01-22 17:02:42 +0000

    Fixed bug withfile perms

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,7 @@ ln -s /buildkit/build/civicrm /var/www/html
 # DRUPAL (7)
 if [ "$CIVITYPE" = "backdrop-demo" ]; then
     cp /buildkit/build/civicrm/sites/default/default.settings.php /buildkit/build/civicrm/sites/default/settings.php
-    mkdir -p  /buildkit/build/civi/sites/default/files
-    chmod 755 /buildkit/build/civi/sites/default/files
-    chmod 755 /buildkit/build/civi/sites/default/settings.php
+    mkdir -p     /buildkit/build/civicrm/sites/default/files
+    chmod 755 -R /buildkit/build/civicrm/sites/default/files
+    chmod 755    /buildkit/build/civicrm/sites/default/settings.php
 fi
